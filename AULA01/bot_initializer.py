b43084a2237646ed675ae5c11956d28646357843
Joyce Carvalho <joycencarvalho@outlook.com>
2026-08-26 23:20:58 -0300

    Lab 01: script de inicialização do bot co variáveis tipadas

--- AULA01/bot_initializer.py
+++ AULA01/bot_initializer.py
@@ -1,4 +1,11 @@
-BOT_NAME = 
-MAX_RETRIES = 
-EXECUTION_TIMEOUT = 
-IS_PRODUCTION = 
+BOT_NAME = "RPA_LAB_01"
+MAX_RETRIES = 3
+EXECUTION_TIMEOUT = 50.5
+IS_PRODUCTION = True
+
+print('-------------------Inicializando-----------------')
+print(f'Nome do Bot: {BOT_NAME} - Tipo: {type(BOT_NAME)}')
+print(f'Nº Máx. Tentativas: {MAX_RETRIES} - Tipo: {type(MAX_RETRIES)}')
+print(f'Tempo limite: {EXECUTION_TIMEOUT} - Tipo: {type(EXECUTION_TIMEOUT)}')
+print(f'Ambiente de produção: {IS_PRODUCTION} - Tipo: {type(IS_PRODUCTION)}')
+print('-------------------------------------------------')
